41c8eede81acc6fe35d7b272be08b4fb6b3b558b
Melanie <m.maddix@gmail.com>
2019-06-20 15:33:44 +0000

    updated migrations and seeds for users, resources, and tags table

--- db/seeds/01_users.js
+++ db/seeds/01_users.js
@@ -1,10 +1,7 @@
 exports.seed = function(knex, Promise) {
-  return knex('users').del()
-    .then(function () {
-      return Promise.all([
-        knex('users').insert({id: 1, name: 'Alice', email: 'alice@sample.com', password: 'password'}),
-        knex('users').insert({id: 2, name: 'Bob', email: 'bob@sample.com', password: 'password'}),
-        knex('users').insert({id: 3, name: 'Charlie', email: 'charlie@sample.com', password: 'password'})
-      ]);
-    });
+  return Promise.all([
+    knex('users').insert({id: 1, name: 'Alice', email: 'alice@sample.com', password: 'password'}),
+    knex('users').insert({id: 2, name: 'Bob', email: 'bob@sample.com', password: 'password'}),
+    knex('users').insert({id: 3, name: 'Charlie', email: 'charlie@sample.com', password: 'password'})
+  ]);
 };
