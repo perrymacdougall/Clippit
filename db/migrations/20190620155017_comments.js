7740bbf1fff1edfe74e508e369f7f091f19647af
Melanie <m.maddix@gmail.com>
2019-06-21 14:24:06 +0000

    fixed seeding and migration files for incremenets and longer comment and resource descriptions

--- db/migrations/20190620155017_comments.js
+++ db/migrations/20190620155017_comments.js
@@ -1,7 +1,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('comments', function (table) {
     table.increments('comment_id');
-    table.string('comment');
+    table.string('comment', 1000);
     table.integer('user_id');
     table.foreign('user_id').references('id').inTable('users');
     table.integer('resource_id');
