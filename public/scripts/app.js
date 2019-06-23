37ff670ff3a8eb0b8b2d6dec452b06f88476ff74
Melanie M <maddixm@users.noreply.github.com>
2019-06-23 17:11:42 -0400

    cleaned up code, removed excess console.log statements, added template with nicer messages for failed registrations and logins (#41)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,3 @@
-// const ENV = process.env.ENV || "development";
-
-// const knexConfig = require("../../knexfile");
-// const knex = require("knex")(knexConfig[ENV]);
-// const morgan = require('morgan');
-// const knexLogger = require('knex-logger');
-
-// const dblikeFunction = require('./public/scripts/likeFunction.js');
-
 $(document).ready(function() {
 
   //jQuery call for like button
@@ -53,7 +44,3 @@ $('.fa-comment-dots').on('click', function () {
 
 
 });
-
-
-
-  
